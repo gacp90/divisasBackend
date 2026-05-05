4ae17c55c1d97fdabc0e555a6070993a020b479f
Alix Lorena Garzón Castillo <135076897+Masterpezcom@users.noreply.github.com>
2026-05-04 19:23:12 -0500

    actualizacion demo final

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ app.use('/api/v1/sources', require('./routes/sources.route'));
 app.use('/api/v1/traslados', require('./routes/traslados.route'));
 app.use('/api/v1/turnos', require('./routes/turnos.route'));
 app.use('/api/v1/uploads', require('./routes/uploads.route'));
+app.use('/api/v1/pagos', require('./routes/pagos.route'));
 
 
 // SPA
